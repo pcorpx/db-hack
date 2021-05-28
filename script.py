3e56f4d4b8c716d8e6fa405a5590cc0801c9d42e
Igor Polezhaev <ingvar@skbkontur.ru>
2021-05-28 19:25:08 +0500

    28.05

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
-import random
+import random, logging, sys
+from datetime import datetime
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+c_handler = logging.StreamHandler(sys.stdout)
+c_handler.setLevel(logging.INFO)
+logger.addHandler(c_handler)
 
 
 def get_child(child_name = "all"):
@@ -10,142 +18,165 @@ def get_child(child_name = "all"):
         child = Schoolkid.objects.get(full_name__contains = child_name)
         return child
     except Schoolkid.DoesNotExist:
-        print("The children with certain name was not found")
+        logger.warning("The children with certain name was not found")
 
 def get_marks(child_name, points__lte = 5, first="no"):
     marks = Mark.objects.filter(points__lte = points__lte, schoolkid=get_child(child_name = child_name))
-    if first="yes":
+    if first=="yes":
         first_mark = marks.first()
         return first_mark
     return marks
 
 def fix_marks(child_name, points__lte = 3, first = "no"):
+
     marks = get_marks(child_name = child_name, points__lte = points__lte, first= first)
-    result = marks.update(points=5)
-    marks.save()
-    print(f"{result} marks were changed")
-    return result
+    if marks and first == "yes":
+        marks.points = 5
+        result = "First mark was changed"
+        logger.info(result)
+        return marks
+    elif marks:
+        result = marks.update(points=5)
+        logger.info(f"{str(result)} marks were changed")
+        return result
+    else:
+        logger.info("There are no bad points")
 
 def get_notes(child_name="all"):
     bad_notes = Chastisement.objects.all()
-    if child_name != "all":s
+    if child_name != "all":
         bad_notes = bad_notes.filter(schoolkid=get_child(child_name=child_name))  
     return bad_notes
 
 def remove_chastisements(child_name):
    bad_notes = get_notes(child_name=child_name)
    result = bad_notes.delete()
-   print(f"{result} notes were deleted")
+   logger.info(f"{result[0]} notes were deleted")
    return result
 
-def get_lessons(year_of_study=None, group_letter=None, subject = None):
+def get_lessons(child_name="all", subject = None):
+    child = get_child(child_name=child_name)
     lessons = Lesson.objects.all()
-    if ((year_of_study is not None) and  (group_letter is not None)):
-        lessons = lessons.filter(year_of_study=child.year_of_study, 
-                                group_letter=child.group_letter)
+    if child_name == "all":
+        return lessons
+    lessons = lessons.filter(year_of_study=child.year_of_study, 
+                             group_letter=child.group_letter)
     if subject != None:
-        lesson = lesson.filter(subject__title=subject)
+        lessons = lessons.filter(subject__title=subject)
     return lessons
 
-def get_lesson(subject, date):
+def get_lesson(child_name, subject, date):
     try:
-        subject_lessons = lessons.filter(subject__title=subject)
+        subject_lessons = get_lessons(child_name, subject)
         lesson = subject_lessons.get(date=date)
         return lesson
     except Lesson.DoesNotExist:
-        print("The lesson of picked subject with certain date was not found")
+        logger.warning("The lesson of picked subject with certain date was not found")
 
-def create_commendation(child_name, subj, words):
+def create_commendation(child_name, subject, words, date = None):
     try:
         child = Schoolkid.objects.get(full_name__contains=child_name)
     except Schoolkid.DoesNotExist:
-        print("The children with certain name was not found")
+        logger.warning("The children with certain name was not found")
         return
-    subj_lessons = Lesson.objects.filter(subject__title=subj, 
+    subject_lessons = Lesson.objects.filter(subject__title=subject, 
                                         year_of_study=child.year_of_study, 
                                         group_letter=child.group_letter).order_by('date')
-    ok = False
-    while not ok:
-        lesson = random.choice(subj_lessons)
-        if len(Commendation.objects.filter(created=lesson.date, subject=lesson.subject, 
-                                    schoolkid=child)) > 0:
-                                    
-            print('Schoolkid already has commendation on this lesson')
-        else:
-            result = Commendation.objects.create(text=random.choice(words), 
-                                                 created=lesson.date, 
-                                                 schoolkid=child,
-                                                 subject=lesson.subject,
-                                                 teacher=lesson.teacher
-                                                 )
-            print(result.text, result.created, result.subject, result.schoolkid)
-            result.save()
-            ok = True
-
-
-
-child = get_child()
-print(child)
-
-all_marks = get_marks(child_name="Фролов Иван")
-print(all_marks)
-
-bad_marks = get_marks(child_name="Фролов Иван", points__lte=3)
-print(bad_marks)
-
-first_mark = get_marks(child_name="Фролов Иван", first="yes")
-
-result = fix_marks(child_name="Фролов Иван", first="yes")
-
-print (first_mark.points)
-
-result = fix_marks(child_name="Фролов Иван")
-
-bad_notes = get_notes()
-print(bad_notes)
-
-bad_notes = get_notes(child_name="Фролов Иван")
-print(bad_notes)
-
-result = remove_chastisements(child_name="Фролов Иван")
-
-result = remove_chastisements(child_name="Голубев Феофан")
-
-all_lessons = get_lessons()
-print(all_lessons)
-
-lessons = get_lessons(year_of_study=child.year_of_study, 
-                      group_letter=child.group_letter)
-
-print(lessons)
-
-math_lessons = get_lessons(year_of_study=child.year_of_study, 
-                           group_letter=child.group_letter,
-                           subject__title="Математика")
-print(math_lessons)
-
-
-lesson = get_lesson("Математика", "2018-10-01")
-    
-
-result = Commendation.objects.create(text="Хвалю", 
+    if (type(words) is str) and date is not None:
+        lesson = subject_lessons.get(date=date)
+        result = Commendation.objects.create(text=words, 
                                         created=lesson.date, 
                                         schoolkid=child,
                                         subject=lesson.subject,
                                         teacher=lesson.teacher
                                         )
-print(result)
+        result.save()
+        created_date = result.created.strftime("%Y-%m-%d")
+        result.created = created_date
+        return result
 
-# Функция для создания похвалы
-words = ["Молодец!",
-         "Отлично!",
-         "Хорошо!",
-         "Гораздо лучше, чем я ожидал!",
-         "Ты меня приятно удивил!",
-         "Великолепно!",
-         "Прекрасно!",
-         "Ты меня очень обрадовал!"]
+    elif (type(words) is list) and date is None:
+        ok = False
+        while not ok:
+            lesson = random.choice(subject_lessons)
+            if len(Commendation.objects.filter(created=lesson.date, subject=lesson.subject, 
+                                        schoolkid=child)) > 0:
+                                        
+                logger.warning('Schoolkid already has commendation on this lesson')
+            else:
+                result = Commendation.objects.create(text=random.choice(words), 
+                                                    created=lesson.date, 
+                                                    schoolkid=child,
+                                                    subject=lesson.subject,
+                                                    teacher=lesson.teacher
+                                                    )
+                result.save()
+                created_date = result.created.strftime("%Y-%m-%d")
+                result.created = created_date
+                ok = True
+                return result
+
+
+def main():
+    child = get_child(child_name="Фролов Иван")
+    logger.info(child)
+
+    all_marks = get_marks(child_name="Фролов Иван")
+    logger.info(all_marks)
+
+    bad_marks = get_marks(child_name="Фролов Иван", points__lte=3)
+    logger.info(bad_marks)
+
+    first_mark = get_marks(child_name="Фролов Иван", points__lte = 3, first="yes")
+    logger.info(first_mark)
+
+    fixed_mark = fix_marks(child_name="Фролов Иван", first="yes")
+    if fixed_mark:
+        logger.info(fixed_mark.points)
+
+    result = fix_marks(child_name="Фролов Иван")
+    logger.info(result)
+
+    bad_notes = get_notes()
+    logger.info(bad_notes)
+
+    bad_notes = get_notes(child_name="Фролов Иван")
+    logger.info(bad_notes)
+
+    result = remove_chastisements(child_name="Фролов Иван")
+    logger.info(result)
+
+    result = remove_chastisements(child_name="Голубев Феофан")
+    logger.info(result)
+
+    all_lessons = get_lessons()
+    logger.info(all_lessons)
+
+    lessons = get_lessons(child_name="Фролов Иван")
+    logger.info(lessons)
+
+    math_lessons = get_lessons(child_name="Фролов Иван",
+                               subject="Математика")
+    logger.info(math_lessons)
+
+
+    lesson = get_lesson("Фролов Иван", "Математика", "2018-10-01")
+    logger.info(lesson)
+        
+    result = create_commendation("Фролов Иван", "Математика", "Хвалю", "2018-10-01")
+    logger.info(f"{result.text}, {result.created}, {result.subject.title}, {result.schoolkid.full_name}")
+
+    words = ["Молодец!",
+            "Отлично!",
+            "Хорошо!",
+            "Гораздо лучше, чем я ожидал!",
+            "Ты меня приятно удивил!",
+            "Великолепно!",
+            "Прекрасно!",
+            "Ты меня очень обрадовал!"]
+
+    result = create_commendation("Фролов Иван", "Музыка", words)
+    logger.info(f"{result.text}, {result.created}, {result.subject.title}, {result.schoolkid.full_name}")
 
 
 
-create_commendation('Фролов Иван', 'Музыка')
