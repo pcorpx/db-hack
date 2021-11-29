1ceb7836a2ce8b067bfd356bc56c4dd32ad66610
pcorpx <pcorpx@gmail.com>
2021-11-29 12:34:12 +0500

    29.11

--- README.md
+++ README.md
@@ -7,43 +7,60 @@ This script was made for studying data manipulation of Django database using  sh
 You can use it by copying content into Django shell directly or putting this script into folder with manage.py and importing it in a shell.
 
 To import script in a shell use command:
-`from script import *`
+
+```python 
+from script import *
+```
 
 ###### Getting all children
-To get all children from db use function get_child wihout arguments (default argument have value "all"):
+To get all children from db use function get_all_children:
 
-`all_chilren = get_child()`
+```python
+all_chilren = get_all_children()
+```
 
 ###### Getting a child
 To get child use function get_child with argument child_name. Use unique name of a child to pass it.
 
-`child = get_child(child_name="Фролов Иван")`
+```python
+child = get_child(child_name="Фролов Иван")
+```
 
 ###### Getting marks
 To get all marks of a child use function get_marks with only one compulsory argument child_name.
 
-`all_marks = get_marks(child_name="Фролов Иван")`
+```python
+all_marks = get_marks(child_name="Фролов Иван")
+```
 
 To get bad marks use function get_marks. You should provide it with two arguments: first positional argument child_name is compulsory, second optional argument point_lte serves to filter marks by their value accordingly less or equal than value passed in the argument. (default value of point_lte is 5)
 
-`bad_marks = get_marks(child_name="Фролов Иван", points__lte=3)`
+```python
+bad_marks = get_marks(child_name="Фролов Иван", points_lte=3)
+```
 
 If you want to get only first mark add an optional argument first with the value of "yes" to the function get_marks:
 
-`first_mark = get_marks(child_name="Фролов Иван", points__lte = 3, first="yes")`
+```python
+first_mark = get_marks(child_name="Фролов Иван", points_lte = 3, first="yes")
+```
 
 ###### Fixing marks
 
 To fix marks use function fix_marks. It has first positional argument child_name which is compulsory and two optional arguments: point_lte (default value is 3) and first (default value is "no").
 To fix all bad marks just provide function with child_name:
 
-`fixed_marks_number = fix_marks(child_name="Фролов Иван")`
+```python
+fixed_marks_number = fix_marks(child_name="Фролов Иван")
+```
 
 The function will return the number of bad marks were fixed.
 
 To fix only first bad mark provide function with optional argument first with the value of "yes":
 
-`fixed_mark = fix_marks(child_name="Фролов Иван", first="yes")`
+```python
+fixed_mark = fix_marks(child_name="Фролов Иван", first="yes")
+```
 
 The function will retun the fixed mark.
 
@@ -51,59 +68,64 @@ The function will retun the fixed mark.
 
 To get all chastisements use function get_notes without arguments:
 
-`bad_notes = get_notes()`
+```python
+bad_notes = get_notes()
+```
 
 To get chastisements of a child use function get_notes() with argument child_name:
 
-`bad_notes = get_notes(child_name="Фролов Иван")`
+```python
+bad_notes = get_notes(child_name="Фролов Иван")
+```
 
 ###### Removing chastisements
 
 To remove chastisments of a child use function remove_chastisements with child_name argument:
 
-`result = remove_chastisements(child_name="Фролов Иван")`
+```python
+removed_chastisements_number = remove_chastisements(child_name="Фролов Иван")
+```
 
 ###### Getting lessons
 
 To get all lessons use functions get_lessons without arguments:
 
-`all_lessons = get_lessons()`
+```python
+all_lessons = get_lessons()
+```
 
 To get lessons of a child provide function with child_name argument:
 
-`lessons = get_lessons(child_name="Фролов Иван")`
+```python
+lessons = get_lessons(child_name="Фролов Иван")
+```
 
 To get child lessons of certain subject use optional argument subject:
 
-`math_lessons = get_lessons(child_name="Фролов Иван", subject="Математика")`
+```python
+math_lessons = get_lessons(child_name="Фролов Иван", subject="Математика")
+```
 
 
 ###### Getting a lesson
 
 To get certain lesson of child use get_lesson function with three positional arguments child_name, subject, date:
 
-`lesson = get_lesson("Фролов Иван", "Математика", "2018-10-01")`
+```python
+lesson = get_lesson("Фролов Иван", "Математика", "2018-10-01")
+```
 
 ###### Creating commendation
 
 To create one commendation for certain lesson use create_commendation fumction with four arguments child_name, subject, words, date:
 
-`result = create_commendation("Фролов Иван", "Математика", "Хвалю", "2018-10-01")`
-
-To create several commendations of a child you should define list of notes named words, than you should use create_commendation function with arguments child_name, subject and words (which was previously defined as a list):
-
+```python
+created_commendation = create_commendation("Фролов Иван", "Математика", "Хвалю", "2018-10-01")
 ```
-words = ["Молодец!",
-         "Отлично!",
-         "Хорошо!",
-         "Гораздо лучше, чем я ожидал!",
-         "Ты меня приятно удивил!",
-         "Великолепно!",
-         "Прекрасно!",
-         "Ты меня очень обрадовал!"]
 
-result = create_commendation("Фролов Иван", "Музыка", words)
-```
+### Checking modifications made by script
+
+Use your browser to follow this address: [127.0.0.1:8000](http://127.0.0.1:8000) and check for data modifications
 
 ### Prerequisites
 
@@ -113,12 +135,6 @@ You should clone and deploy e-diary using instructions in README there https://g
 
 To start Django shell just use:
 
-`python manage.py shell`
-
-## Tests
-
-To pass all tasks of the module use function:
-
-`main()`
-
-Use your browser to follow this address: [127.0.0.1:8000](http://127.0.0.1:8000) and check for data modifications
+```bash
+python manage.py shell
+```
--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 import sys
-import random
 import logging
 
 from datacenter.models import Schoolkid, Mark, Chastisement
@@ -13,28 +12,34 @@ c_handler.setLevel(logging.INFO)
 logger.addHandler(c_handler)
 
 
-def get_child(child_name="all"):
-    if child_name == "all":
-        all_children = Schoolkid.objects.all()
-        return all_children
+def get_all_children():
+    all_children = Schoolkid.objects.all()
+    return all_children
+
+
+def get_child(child_name):
     try:
         child = Schoolkid.objects.get(full_name__contains=child_name)
         return child
+    except Schoolkid.MultipleObjectsReturned:
+        logger.warning("There were several children found with defined name")
     except Schoolkid.DoesNotExist:
         logger.warning("The children with certain name was not found")
 
 
-def get_marks(child_name, points__lte=5, first="no"):
-    marks = Mark.objects.filter(points__lte=points__lte, 
-                                schoolkid=get_child(child_name=child_name))
-    if first == "yes":
-        first_mark = marks.first()
-        return first_mark
-    return marks
+def get_marks(child_name, points_lte=5, first="no"):
+    child = get_child(child_name=child_name)
+    if child:
+        marks = Mark.objects.filter(points__lte=points_lte,
+                                    schoolkid=child)
+        if first == "yes":
+            first_mark = marks.first()
+            return first_mark
+        return marks
 
 
-def fix_marks(child_name, points__lte=3, first="no"):
-    marks = get_marks(child_name=child_name, points__lte=points__lte, 
+def fix_marks(child_name, points_lte=3, first="no"):
+    marks = get_marks(child_name=child_name, points_lte=points_lte,
                       first=first)
     if marks and first == "yes":
         marks.points = 5
@@ -51,88 +56,83 @@ def fix_marks(child_name, points__lte=3, first="no"):
 def get_chastisements(child_name="all"):
     chastisements = Chastisement.objects.all()
     if child_name != "all":
-        chastisements = chastisements.filter(
-            schoolkid=get_child(child_name=child_name))  
+        child = get_child(child_name=child_name)
+        if child:
+            chastisements = chastisements.filter(
+                schoolkid=child)
+        else:
+            return
     return chastisements
 
 
 def remove_chastisements(child_name):
-   chastisements = get_chastisements(child_name=child_name)
-   removed_chastisements_number = chastisements.delete()
-   logger.info(f"{removed_chastisements_number[0]} \
-               chastisements were deleted")
-   return removed_chastisements_number
+    chastisements = get_chastisements(child_name=child_name)
+    if chastisements:
+        removed_chastisements_number = chastisements.delete()
+        removed_chastisements_number = removed_chastisements_number[0]
+        message_info = (
+            f"{removed_chastisements_number} "
+            f"chastisements were removed"
+        )
+    else:
+        removed_chastisements_number = 0
+        message_info = "There were no chastisement removed"
+    logger.info(message_info)
+    return removed_chastisements_number
 
 
 def get_lessons(child_name="all", subject=None):
     child = get_child(child_name=child_name)
-    lessons = Lesson.objects.all()
-    if child_name == "all":
+    if child:
+        lessons = Lesson.objects.all()
+        if child_name == "all":
+            return lessons
+        lessons = lessons.filter(year_of_study=child.year_of_study,
+                                 group_letter=child.group_letter)
+        if subject:
+            lessons = lessons.filter(subject__title=subject)
         return lessons
-    lessons = lessons.filter(year_of_study=child.year_of_study, 
-                             group_letter=child.group_letter)
-    if subject != None:
-        lessons = lessons.filter(subject__title=subject)
-    return lessons
 
 
 def get_lesson(child_name, subject, date):
     try:
         subject_lessons = get_lessons(child_name, subject)
-        lesson = subject_lessons.get(date=date)
-        return lesson
+        if subject_lessons:
+            lesson = subject_lessons.get(date=date)
+            return lesson
     except Lesson.DoesNotExist:
         logger.warning("The lesson of picked subject with \
                        certain date was not found")
 
 
-def create_commendation(child_name, subject, commendation_text, date=None):
+def create_commendation(child_name, subject, commendation_content, date=None):
     try:
         child = Schoolkid.objects.get(full_name__contains=child_name)
+    except Schoolkid.MultipleObjectsReturned:
+        logger.warning("There were several children found with defined name")
+        return
     except Schoolkid.DoesNotExist:
         logger.warning("The children with certain name was not found")
         return
     subject_lessons = Lesson.objects.filter(
-        subject__title=subject, 
-        year_of_study=child.year_of_study, 
-        group_letter=child.group_letter).order_by('date')
-                                            
-    if (type(commendation_text) is str) and date is not None:
+        subject__title=subject,
+        year_of_study=child.year_of_study,
+        group_letter=child.group_letter
+    ).order_by('date')
+
+    if (isinstance(commendation_content, str) and date):
         lesson = subject_lessons.get(date=date)
         created_commendation = Commendation.objects.create(
-            text=commendation_text, 
-            created=lesson.date, 
+            text=commendation_content,
+            created=lesson.date,
             schoolkid=child,
             subject=lesson.subject,
-            teacher=lesson.teacher)
+            teacher=lesson.teacher
+        )
         created_commendation.save()
         created_date = created_commendation.created.strftime("%Y-%m-%d")
         created_commendation.created = created_date
         return created_commendation
-
-    elif (type(commendation_text) is list) and (date is None):
-        ok = False
-        while not ok:
-            lesson = random.choice(subject_lessons)
-            if len(Commendation.objects.filter(created=lesson.date, 
-                                               subject=lesson.subject, 
-                                               schoolkid=child)) > 0:
-                                        
-                logger.warning("Schoolkid already has commendation on \
-                               this lesson")
-            else:
-                created_commendation = Commendation.objects.create(
-                    text=random.choice(commendation_text), 
-                    created=lesson.date, 
-                    schoolkid=child,
-                    subject=lesson.subject,
-                    teacher=lesson.teacher)
-                created_commendation.save()
-                created_date = created_commendation.created
-                created_date = created_date.strftime("%Y-%m-%d")
-                created_commendation.created = created_date
-                ok = True
-                return created_commendation
 
 
 def main():
@@ -142,10 +142,10 @@ def main():
     all_marks = get_marks(child_name="Фролов Иван")
     logger.info(all_marks)
 
-    bad_marks = get_marks(child_name="Фролов Иван", points__lte=3)
+    bad_marks = get_marks(child_name="Фролов Иван", points_lte=3)
     logger.info(bad_marks)
 
-    first_mark = get_marks(child_name="Фролов Иван", points__lte=3, 
+    first_mark = get_marks(child_name="Фролов Иван", points_lte=3,
                            first="yes")
     logger.info(first_mark)
 
@@ -159,14 +159,18 @@ def main():
     chastisements = get_chastisements()
     logger.info(chastisements)
 
-    chastisements= get_chastisements(child_name="Фролов Иван")
+    chastisements = get_chastisements(child_name="Фролов Иван")
     logger.info(chastisements)
 
-    result = remove_chastisements(child_name="Фролов Иван")
-    logger.info(result)
+    removed_chastisements_number = remove_chastisements(
+        child_name="Фролов Иван"
+    )
+    logger.info(removed_chastisements_number)
 
     removed_chastisements_number = remove_chastisements(
-        child_name="Голубев Феофан")
+        child_name="Голубев Феофан"
+    )
+
     logger.info(removed_chastisements_number)
 
     all_lessons = get_lessons()
@@ -179,33 +183,17 @@ def main():
                                subject="Математика")
     logger.info(math_lessons)
 
-
     lesson = get_lesson("Фролов Иван", "Математика", "2018-10-01")
     logger.info(lesson)
-        
+
     created_commendation = create_commendation("Фролов Иван", "Математика",
                                                "Хвалю", "2018-10-01")
-    logger.info(f"{created_commendation.text}, {created_commendation.created}, 
-                {created_commendation.subject.title}, 
-                {created_commendation.schoolkid.full_name}")
 
-    commendation_text = [
-        "Молодец!",
-        "Отлично!",
-        "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!",
-        "Великолепно!",
-        "Прекрасно!",
-        "Ты меня очень обрадовал!"
-    ]
+    message_info = (
+        f"{created_commendation.text}, "
+        f"{created_commendation.created}, "
+        f"{created_commendation.subject.title}, "
+        f"{created_commendation.schoolkid.full_name}"
+    )
 
-    created_commendation = create_commendation("Фролов Иван", "Музыка", 
-                                               commendation_text)
-    logger.info(f"{created_commendation.text}, 
-                {created_commendation.created}, 
-                {created_commendation.subject.title}, 
-                {created_commendation.schoolkid.full_name}")
-
-
-
+    logger.info(message_info)
